c349c2ad6721dd514a99bf82b70235e8203ff9c9
daisuke.tsuruta <dtsuruta@planetnine.co.jp>
2020-11-05 19:20:52 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Daisuke Tsuruta on 2020/11/05.
+//  Copyright © 2020 daisuke.tsuruta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
